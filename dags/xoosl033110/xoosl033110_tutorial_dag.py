9e37cd6b2801666e1074ab0d98723e72802af8f6
맞갖다 <xoosl033110@gmail.com>
2026-01-12 15:38:09 +0900

    U tutorial

---
+++ dags/xoosl033110/xoosl033110_tutorial_dag.py
@@ -0,0 +1,27 @@
+import pendulum
+from airflow import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+default_args = dict(
+    owner = 'xoosl033110', # 개별 DAG 관리자
+    email = ['xoosl033110@gmail.com'],
+    email_on_failure = False,
+    retries = 3
+    )
+
+with DAG(
+    dag_id="xoosl033110_tutorial_dag",
+    start_date=pendulum.datetime(2025, 8, 1, tz='Asia/Seoul'),
+    schedule="30 10 * * *", # cron 표현식
+    default_args = default_args,
+    catchup=False
+):
+    
+    task1 = EmptyOperator(task_id="task1")
+    task2 = EmptyOperator(task_id="task2")
+    task3 = EmptyOperator(task_id="task3")
+    task4 = EmptyOperator(task_id="task4")
+    task5 = EmptyOperator(task_id="task5")
+
+# task1 >> task2 >> task3 >> task4 >> task5
+task1 >> [task2, task3] >> task4 >> task5
