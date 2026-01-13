4643cc96b030b7a27588e78735368843a473ee9a
seungwoo <136046385+walter230@users.noreply.github.com>
2026-01-13 15:33:57 +0900

    Set subway monitor DAG schedule to every 5 minutes

--- dags/ansemr99/ansemr99_seoul_subway_monitor.py
+++ dags/ansemr99/ansemr99_seoul_subway_monitor.py
@@ -24,7 +24,7 @@ default_args = dict(
 with DAG(
     dag_id="ansemr99_seoul_subway_monitor",
     start_date=pendulum.today('Asia/Seoul').add(days=-1),
-    schedule=None, # "*/5 * * * *",  # 5분마다 실행
+    schedule="*/5 * * * *", # 5분마다 실행
     catchup=False,
     default_args=default_args,
     tags=['subway', 'project', 'slack'],
